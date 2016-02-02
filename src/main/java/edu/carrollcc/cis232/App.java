5de12e78b99e5beec3a3822a38273b3fd63abc69
Paul Turnbaugh <pet30200@gmail.com>
2016-02-01 20:46:20 -0500

    My work on the example lab. Got hello name working.

--- src/main/java/edu/carrollcc/cis232/App.java
+++ src/main/java/edu/carrollcc/cis232/App.java
@@ -1,5 +1,7 @@
 package edu.carrollcc.cis232;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+    	Scanner keyboard = new Scanner(System.in);
+    	System.out.println("Whats your name?");
+    	String name = keyboard.nextLine();
+        System.out.printf("Hello %s!%n", name);
+        keyboard.close();
     }
 }
